24a95c117193d8ac68f7dcf3050f8728fc78dad8
nornjuidat <njuidatnor750@gmail.com>
2024-11-29 09:57:17 +0200

    בסיס מעקבים

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+//npm i express body-parser
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
+const port = 6183;
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+const path = require("path");
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port http://localhost:${port}`);
+});
