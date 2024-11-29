64eda8f9dcd77a891d31f26a30b8c8c68dc8418a
nornjuidat <njuidatnor750@gmail.com>
2024-11-29 12:34:35 +0200

    פעולה ראשונה עם בסיס נתונים

--- Routers/course_R.js
+++ Routers/course_R.js
@@ -6,6 +6,14 @@ let courses=[];
 
 router.post("/courses", (req, res) => { //Create - הוספה
     let course_name   = req.body.course_name;
+    const Query = `INSERT INTO courses (name) VALUES('${course_name}')`;
+    db_pool.query(Query,function (err,rows,fields,){
+        if (err){
+            res.status(500).json({message:err});
+        }else{
+            res.status(200).json({message:"OK",Last_Id:rows.insertId});
+        }
+    })
     courses.push({name:course_name});
     res.status(200).json("ok");
 });
